767334412de0b906223c5abd3307cade863d34f8
ccseyhan <cemalcan.seyhan@deop.ca>
2023-01-23 14:00:45 -0300

    remote_exec added

--- main.tf
+++ main.tf
@@ -86,6 +86,20 @@ resource "azurerm_virtual_machine" "vm1" {
       key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYBkJ3y1tV6p8KApdM+Z97qYqO8QMTdL66B3ZRFA/MQ6q6FutPI4uyCTWbSB3z6vQU0OqfkJhSRElgUbBVM8aCa+we5CSa9Cr5AuM2MCJlelZTnfBAOUY1CaIRlvXB5jNLEplRpY2wnh2S6yRMbD72U6cE/zPW0Zc+0uu2ffzdy2wxhc0vAoLn3h5OwgZ1NoGsZMPQYZyPuxlfeShaC7EXqRDXvcRZKQktElnSQcyRN14glgt+AwYUFYF7vjxju2HnVsgJi8saPtEKvYETwkwmhs9KZJOavxI8+hveoSaFN5zMhuFIVDAo5G1LtmsDir7ibAGG4XZuuCMLhlGUxZLBJt7+AvG7AnjJnigBnAhaC1LGG3HKpkZ/YxQaZnv2vWiWqohD+FTY8uDzjug2eK0Y+hNyKoib1KfIClXoKfAINUn5RH5zxWxFsBALbVIcIR4hMY7Y0Ul5rE+eAOjai9VS7nkNci6QOO7tnbiS/VaZiqevlOrJJoGG0U2D37lLLKukNc1KXHAU108iL/TuA77s5NA/rQykOxn1LAlL8Z7LcUV6fKEg4NaXOxiCaC6zU1tZBy1okcSfOK0fSyw6Ymq2/QJsjdEIkdQz4SEyWotQLPLIPZKJHHle+NMyfm+7tkaINdm5FMYZi4bQ8L34ctgVXA2RRJuSZPE+p2y8qh8I5Q=="
     }
   }
+
+  connection {
+      type        = "ssh"
+      host        = azurerm_public_ip.pip1.ip_address
+      user        = var.admin_username
+      password    = var.admin_password
+    }
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum install cifs-utils -y",
+        "sudo mkdir -p /mnt/${azurerm_storage_account.example.name}/${azurerm_storage_share.example.name}",
+        "sudo mount -t cifs //${azurerm_storage_account.example.name}.file.core.windows.net/${azurerm_storage_share.example.name} /mnt/${azurerm_storage_account.example.name}/${azurerm_storage_share.example.name} -o vers=3.0,dir_mode=0777,file_mode=0777,serverino,username=${azurerm_storage_account.example.name},password=${azurerm_storage_account.example.primary_access_key}",
+    ]
+  }
 }
 
 
@@ -121,6 +135,18 @@ resource "azurerm_network_security_group" "nsg1" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "Allowsmb"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "445"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
@@ -147,18 +173,18 @@ resource "azurerm_network_security_group" "nsg1" {
 # ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
 
 
-# resource "azurerm_storage_account" "sa1" {
-#   name                     = "ccseyhantest1234"
-#   resource_group_name      = azurerm_resource_group.rg1.name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "sa1" {
+  name                     = "ccseyhantest1234"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-# resource "azurerm_storage_share" "example" {
-#   name                 = "sharename"
-#   storage_account_name = azurerm_storage_account.sa1.name
-#   quota                = 50
+resource "azurerm_storage_share" "example" {
+  name                 = "sharename"
+  storage_account_name = azurerm_storage_account.sa1.name
+  quota                = 50
 
-# }
+}
 
--- test.tf
+++ test.tf
@@ -1,74 +1,68 @@
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "example" {
-  name                = "ccseyhankeys1234"
-  location            = azurerm_resource_group.rg1.location
-  resource_group_name = azurerm_resource_group.rg1.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+# resource "azurerm_key_vault" "example" {
+#   name                = "ccseyhankeys1234"
+#   location            = azurerm_resource_group.rg1.location
+#   resource_group_name = azurerm_resource_group.rg1.name
+#   tenant_id           = data.azurerm_client_config.current.tenant_id
+#   sku_name            = "standard"
 
-  purge_protection_enabled = true
-}
+#   purge_protection_enabled = true
+# }
 
-resource "azurerm_key_vault_access_policy" "storage" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_storage_account.example.identity.0.principal_id
+# resource "azurerm_key_vault_access_policy" "storage" {
+#   key_vault_id = azurerm_key_vault.example.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = azurerm_storage_account.example.identity.0.principal_id
 
-  key_permissions    = ["Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
-  secret_permissions = ["Get"]
-}
+#   key_permissions    = ["Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
+#   secret_permissions = ["Get"]
+# }
 
-resource "azurerm_key_vault_access_policy" "client" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+# resource "azurerm_key_vault_access_policy" "client" {
+#   key_vault_id = azurerm_key_vault.example.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azurerm_client_config.current.object_id
 
-  key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
-  secret_permissions = ["Get"]
-}
-
-
-resource "azurerm_key_vault_key" "example" {
-  name         = "tfex-key"
-  key_vault_id = azurerm_key_vault.example.id
-  key_type     = "RSA"
-  key_size     = 2048
-  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
-
-  depends_on = [
-    azurerm_key_vault_access_policy.client,
-    azurerm_key_vault_access_policy.storage,
-  ]
-}
+#   key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
+#   secret_permissions = ["Get"]
+# }
 
 
-resource "azurerm_storage_account" "example" {
-  name                     = "ccseyhan12345"
-  resource_group_name      = azurerm_resource_group.rg1.name
-  location                 = azurerm_resource_group.rg1.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# resource "azurerm_key_vault_key" "example" {
+#   name         = "tfex-key"
+#   key_vault_id = azurerm_key_vault.example.id
+#   key_type     = "RSA"
+#   key_size     = 2048
+#   key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
+#   depends_on = [
+#     azurerm_key_vault_access_policy.client,
+#     azurerm_key_vault_access_policy.storage,
+#   ]
+# }
 
-resource "azurerm_storage_account_customer_managed_key" "example" {
-  storage_account_id = azurerm_storage_account.example.id
-  key_vault_id       = azurerm_key_vault.example.id
-  key_name           = azurerm_key_vault_key.example.name
-}
 
-resource "azurerm_storage_share" "example" {
-  name                 = "sharename"
-  storage_account_name = azurerm_storage_account.example.name
-  quota                = 50
-}
+# resource "azurerm_storage_account" "example" {
+#   name                     = "ccseyhan12345"
+#   resource_group_name      = azurerm_resource_group.rg1.name
+#   location                 = azurerm_resource_group.rg1.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-resource "azurerm_storage_mount" "example" { 
-    storage_share_name = azurerm_storage_share.example.name
-    storage_account_name = azurerm_storage_account.example.name
-    file_path = "/path/to/mount/point"
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
+
+# resource "azurerm_storage_account_customer_managed_key" "example" {
+#   storage_account_id = azurerm_storage_account.example.id
+#   key_vault_id       = azurerm_key_vault.example.id
+#   key_name           = azurerm_key_vault_key.example.name
+# }
+
+# resource "azurerm_storage_share" "example" {
+#   name                 = "sharename"
+#   storage_account_name = azurerm_storage_account.example.name
+#   quota                = 50
+# }
